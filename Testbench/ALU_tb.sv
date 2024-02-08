a4ca0a9855b3d428f2814dfc08c3e8e2f26d1908
Hoàng Bảo Long <105693455+106210049@users.noreply.github.com>
2024-02-08 18:28:40 +0700

    Update ALU_tb.sv

--- Testbench/ALU_tb.sv
+++ Testbench/ALU_tb.sv
@@ -1,25 +1,24 @@
 // Code your testbench here
 // or browse Examples
-// Code your testbench here
-// or browse Examples
+
 module ALU_tb;
 import CPU_package::*;
   reg [DATA_WIDTH-1:0] 	in_a;
   reg [DATA_WIDTH-1:0] 	in_b;
-  reg					          input_carry;
-  enum_alu_opcode_t 	  alu_opcode;
-  reg 				          alu_mode;
+  reg					input_carry;
+  enum_alu_opcode_t 	alu_opcode;
+  reg 				    alu_mode;
   wire [DATA_WIDTH-1:0] alu_out;
-  struct_alu_flag_t 	  alu_out_flag; 
+  struct_alu_flag_t 	alu_out_flag; 
 
   ALU alu(
-    		    .in_a(in_a),
+    		.in_a(in_a),
             .in_b(in_b),
             .input_carry(input_carry),
             .alu_opcode(alu_opcode),
-        		.alu_mode(alu_mode),
-        		.alu_out(alu_out),
-        		.alu_out_flag(alu_out_flag)
+    		.alu_mode(alu_mode),
+    		.alu_out(alu_out),
+    		.alu_out_flag(alu_out_flag)
   );
   
   initial begin
@@ -32,7 +31,7 @@ import CPU_package::*;
   end
   
   initial begin
-    alu_mode=0;
+    alu_mode=1;
     in_a=1;
     in_b=0;
     alu_opcode=ALU_OP_AND;
@@ -42,31 +41,31 @@ import CPU_package::*;
     in_b=1;
     alu_opcode=ALU_OP_AND;
     #20
-    alu_mode=0;
+    alu_mode=1;
     in_a=1;
     in_b=0;
     alu_opcode=ALU_OP_OR;
     #20
-    alu_mode=0;
+    alu_mode=1;
     in_a=16'd2;
     in_b=16'd2;
     alu_opcode=ALU_OP_CPR;
     #20
-    alu_mode=0;
+    alu_mode=1;
     in_a=16'd20;
     in_b=16'd10;
     alu_opcode=ALU_OP_CPR;
     #20
-    alu_mode=0;
+    alu_mode=1;
     in_a=16'd2;
     in_b=16'd10;
     alu_opcode=ALU_OP_CPR;
     #20
     in_a=0;
     in_b=0;
-    alu_mode=1;
+    alu_mode=0;
     #5
-    alu_mode=1;
+    alu_mode=0;
     alu_opcode=ALU_OP_ADD;
     for (int i=0;i<16;i=i+1) begin
       #20
@@ -78,13 +77,23 @@ import CPU_package::*;
       end
     end
     #20
-    alu_mode=1;
+    alu_mode=0;
     in_a=16'd4;
     alu_opcode=ALU_OP_SHR;
     #20
-    alu_mode=1;
+    alu_mode=0;
     in_a=16'd4;
     alu_opcode=ALU_OP_SHL;
+    #20
+    alu_mode=0;
+    in_a=16'd2;
+    in_b=16'd2;
+    alu_opcode=ALU_OP_MUL;
+    #20
+    alu_mode=0;
+    in_a=16'd10;
+    in_b=16'd2;
+    alu_opcode=ALU_OP_DIV;
     #20
     $finish;
   end
